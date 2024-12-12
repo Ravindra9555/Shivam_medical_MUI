1e1a8431d1b9d38271f8f555e8489ad9a6ef7f3b
Ravindra Kumar <ravindraietbu@gmail.com>
2024-12-12 23:23:10 +0530

    fix

---
+++ src/pages/Admin/AdminAppomtmentGraph.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AdminAppomtmentGraph = () => {
+  return (
+    <div>
+      <>
+       Appointmnet chart  
+       
+       </>
+    </div>
+  )
+}
+
+export default AdminAppomtmentGraph
